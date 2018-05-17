85cccf2c5c242c17c80e848e57b68f31f1681f72
muwenzi <mrgaonju@gmail.com>
2018-05-18 00:22:37 +0800

    refactor: remove UglifyJsPlugin and version of entry name

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
-const fs = require('fs')
+// const fs = require('fs')
 const rimraf = require('rimraf')
-const webpack = require('webpack')
+// const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const version = JSON.parse(fs.readFileSync('package.json')).version
+// const version = JSON.parse(fs.readFileSync('package.json')).version
 const sourcePath = path.resolve(__dirname, '../src')
-const outputPath = path.resolve(__dirname, '../dist/libs')
-const entryName = `earth-ui-${version}.min`
+const outputPath = path.resolve(__dirname, '../dist')
+// const entryName = `earth-ui-${version}.min`
+const entryName = `earth-ui`
 
 rimraf.sync(outputPath)
 
@@ -38,7 +39,7 @@ const config = {
       include: sourcePath
     }, {
       test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-      use: ['url-loader?limit=100000']
+      use: ['url-loader']
     }]
   },
   resolve: {
@@ -54,12 +55,12 @@ const config = {
     'prop-types': 'PropTypes'
   },
   plugins: [
-    new ExtractTextPlugin('[name].css'),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        properties: false
-      }
-    })
+    new ExtractTextPlugin('[name].css')
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     properties: false
+    //   }
+    // })
   ]
 }
 
