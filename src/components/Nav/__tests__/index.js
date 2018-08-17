f6374687326ad6f6e96167caf4c0df17a784b3a5
muwenzi <mrgaonju@gmail.com>
2018-08-17 15:58:56 +0800

    fix(Nav/test): add SubNav in nav test

--- src/components/Nav/__tests__/index.js
+++ src/components/Nav/__tests__/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { findDOMNode } from 'react-dom'
 import TestUtils from 'react-dom/test-utils'
-import { Nav, NavItem } from '../index'
+import { Nav, SubNav, NavItemGroup, NavItem } from '../index'
 
 // Object.defineProperty(window.location, 'href', {
 //   writable: true,
@@ -15,10 +15,12 @@ describe('Nav', () => {
     })
     const instance = TestUtils.renderIntoDocument(
       <Nav selectedId="1">
-        <NavItem id="2" defaultOpen>
-          <NavItem id="3" />
-          <NavItem id="4" />
-        </NavItem>
+        <SubNav id="2" defaultOpen>
+          <NavItemGroup>
+            <NavItem id="3" />
+            <NavItem id="4" />
+          </NavItemGroup>
+        </SubNav>
       </Nav>
     )
     expect(findDOMNode(instance).querySelector('li').className).toContain('open')
