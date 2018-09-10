359ec67fb60c7e7063feccb5af19fcadc644bbfb
muwenzi <mrgaonju@gmail.com>
2018-09-11 00:07:01 +0800

    fix(Notfound): back issue in notfound page

--- site/pages/NotFound/index.js
+++ site/pages/NotFound/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropType from 'prop-types'
 import Button from 'earth-ui/lib/Button'
 import ScrollUp from 'widgets/ScrollUp'
 import './index.less'
@@ -10,14 +9,10 @@ const NotFound = (props) => {
       <div className="not-found">
         <h1>404</h1>
         <p>您访问的页面不存在，也可能被移除了</p>
-        <Button onClick={() => props.history.goBack()}>返回</Button>
+        <Button onClick={() => window.history.back()}>返回</Button>
       </div>
     </ScrollUp>
   )
-}
-
-NotFound.propTypes = {
-  history: PropType.object
 }
 
 export default NotFound
