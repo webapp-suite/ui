7b37ef8d96030616fd4b2552fb39501f9020d30a
muwenzi <mrgaonju@gmail.com>
2018-09-11 17:27:47 +0800

    fix(gulpfile): cannot use lib caused by prefixCls config

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -4,5 +4,6 @@ const replace = require('gulp-replace')
 gulp.task('default', function () {
   gulp.src('../lib/components/**/*.js')
     .pipe(replace(/require\('(\.\/)*(\.\.\/)*(\w+\/)*\w+\.less'\);/, ''))
+    .pipe(replace(/prefixCls/g, JSON.stringify('earthui')))
     .pipe(gulp.dest('../lib'))
 })
