782e10dc3474b4926f476b355d8e0686bb8154ab
muwenzi <mrgaonju@gmail.com>
2018-10-29 10:16:15 +0800

    use new icon

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
-import '../../styles/iconfont.less'
+import '../../styles/ui-icons.less'
 
 const Icon = props => {
   const { className, type, ...other } = props
   return (
-    <i className={cx(`${prefixCls}-icon icon`, 'icon-' + type, className)} {...other} />
+    <i className={cx(`${prefixCls}-icon`, `${prefixCls}-icon__${type}`, className)} {...other} />
   )
 }
 
 Icon.propTypes = {
   className: PropTypes.string,
-  // 图标类型，https://fontawesome.com/v4.7.0/icons/
+  // 图标类型
   type: PropTypes.string.isRequired
 }
 
