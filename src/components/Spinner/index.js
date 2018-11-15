d1d825c52f902b059438fe92f3e277113cf7b7bf
muwenzi <mrgaonju@gmail.com>
2018-11-15 13:37:52 +0800

    add basic spinner

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
 import cx from 'classnames'
+import PropTypes from 'prop-types'
+import React from 'react'
 import './index.less'
 
 const Spinner = props => {
-  const { className, style, height, ...other } = props
+  const { className, style, height, isLoading, children, ...other } = props
 
   if (typeof height !== 'undefined') {
     other.style = Object.assign(style || {}, {
@@ -13,18 +13,31 @@ const Spinner = props => {
   }
 
   return (
-    <div className={cx(`${prefixCls}-spinner`, className)} {...other}>
-      <div className={`${prefixCls}-spinner__circle`} />
+    <div className={cx(`${prefixCls}-spinner`, {[`${prefixCls}-spinner_loading`]: isLoading}, className)} {...other}>
+      {isLoading && (
+        <div className={`${prefixCls}-spinner__container`}>
+          <div className={`${prefixCls}-spinner__container-spinner`} />
+        </div>
+      )}
+      {children}
     </div>
   )
 }
 
 Spinner.propTypes = {
   className: PropTypes.string,
+  children: PropTypes.node,
   style: PropTypes.object,
 
   // 高度，默认 30px，宽度与高度相同
-  height: PropTypes.number
+  height: PropTypes.number,
+  tip: PropTypes.string,
+  isLoading: PropTypes.bool,
+  cover: PropTypes.oneOf(['white', 'black'])
+}
+
+Spinner.defaultProps = {
+  cover: 'white'
 }
 
 export default Spinner
