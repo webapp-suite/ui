4852a33faad8ce3cb39b0c8cd2cad56f3337a9e8
muwenzi <mrgaonju@gmail.com>
2019-01-02 00:27:19 +0800

    add in progress page

---
+++ site/apps/InProgress/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.less'
+
+const NotFound = (props) => {
+  return (
+    <div className="in-progress">
+      <div className="in-progress__image" />
+      <p>Work in progress...</p>
+    </div>
+  )
+}
+
+export default NotFound
