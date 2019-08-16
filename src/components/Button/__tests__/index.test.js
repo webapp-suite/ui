36a51cd13b0d70a74801a41bde7981d5bc3c5805
Kimi Gao <mrgaonju@gmail.com>
2019-08-16 14:34:54 +0800

    test(Button): add unit test and snapshot test in Button

---
+++ src/components/Button/__tests__/index.test.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { render, mount } from 'enzyme'
+import renderer from 'react-test-renderer'
+import Button from '../index'
+import Icon from '../../Icon'
+
+describe('Button', () => {
+  it('should render correctly', () => {
+    const wrapper = render(<Button>OK</Button>)
+    expect(wrapper).toMatchSnapshot()
+  })
+  it('should mount correctly', () => {
+    expect(() => renderer.create(<Button>OK</Button>)).not.toThrow()
+  })
+  it('should render className correctly', () => {
+    const wrapper = mount(<Button className="test">OK</Button>)
+    expect(wrapper.find('button').hasClass('test')).toBe(true)
+  })
+  it('should render style correctly', () => {
+    const wrapper = render(<Button style={{ color: 'red' }}>OK</Button>)
+    expect(wrapper.prop('style').color).toBe('red')
+  })
+  it('should render Chinese characters correctly', () => {
+    const wrapper = render(<Button>添加</Button>)
+    expect(wrapper).toMatchSnapshot()
+  })
+  it('should render icon correctly', () => {
+    const wrapper1 = render(<Button icon="add">添加</Button>)
+    expect(wrapper1).toMatchSnapshot()
+    const wrapper2 = render(
+      <Button>
+        <Icon type="add" />
+        添加
+      </Button>
+    )
+    expect(wrapper2).toMatchSnapshot()
+  })
+  it('should trigger event function correctly', () => {
+    const handleClick = jest.fn()
+    const wrapper = mount(<Button onClick={handleClick}>OK</Button>)
+    wrapper.simulate('click')
+    expect(handleClick).toBeCalled()
+    expect(handleClick.mock.calls.length).toBe(1)
+  })
+  it('should render disabled button correctly', () => {
+    const wrapper = render(<Button disabled>OK</Button>)
+    expect(wrapper.prop('disabled')).toBe(true)
+  })
+})
