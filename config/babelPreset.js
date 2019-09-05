8df7bf6f832d83e05aa284b1e0a7f13698569b0d
Kimi Gao <mrgaonju@gmail.com>
2019-09-05 15:13:01 +0800

    optimize babel config

--- config/babelPreset.js
+++ config/babelPreset.js
@@ -4,17 +4,12 @@ const building = BABEL_ENV !== undefined && BABEL_ENV !== 'cjs'
 module.exports = () => ({
   presets: [
     [
-      '@babel/env',
+      '@babel/preset-env',
       {
         loose: true,
         modules: building ? false : 'commonjs'
       }
     ],
     '@babel/preset-react'
-  ],
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    ['@babel/plugin-proposal-class-properties', { loose: false }],
-    ['@babel/plugin-proposal-optional-chaining', { loose: false }]
   ]
 })
